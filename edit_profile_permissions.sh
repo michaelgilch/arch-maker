e879e66a26ecbc3df6c892feec1e090a62082b15
Michael Gilchrist <michaelgilch@gmail.com>
2021-02-24 10:43:43 -0500

    Adjust permissions for recursive folder changes
    Requires custom build of mkarchiso via gitlab archiso

--- edit_profile_permissions.sh
+++ edit_profile_permissions.sh
@@ -5,11 +5,14 @@
 sed -i '/file_permissions/a ["/root/install.sh"]="0:0:755"' build/releng/profiledef.sh
 sed -i '/file_permissions/a ["/root/partition.sh"]="0:0:755"' build/releng/profiledef.sh
 sed -i '/file_permissions/a ["/root/post-install.sh"]="0:0:755"' build/releng/profiledef.sh
+
+# Use custom build of mkarchiso via dev/gitlab/archiso
+# to allow recursive setting of permissions in a folder
 sed -i '/file_permissions/a ["/root/post-install"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/00-verify-mounts.sh"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/10-home.sh"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/20-packages.sh"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/30-aur-packages.sh"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/40-other-packages.sh"]="0:0:755"' build/releng/profiledef.sh
-sed -i '/file_permissions/a ["/root/post-install/50-secrets.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/00-verify-mounts.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/05-services.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/10-home.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/30-aur-packages.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/40-other-packages.sh"]="0:0:755"' build/releng/profiledef.sh
+#sed -i '/file_permissions/a ["/root/post-install/50-secrets.sh"]="0:0:755"' build/releng/profiledef.sh
 
