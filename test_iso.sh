3c915e7d03029d46083e449494570e07d177271d
Michael Gilchrist <michaelgilch@gmail.com>
2021-03-10 15:36:13 -0500

    Add optional argument allowing specification of iso to test
    - if no argument is provided, use the latest

--- test_iso.sh
+++ test_iso.sh
@@ -2,4 +2,10 @@
 
 # Tests ISO by running the image in QEMU
 
-run_archiso -i bin/archlinux-2021.02.23-x86_64.iso
+if [ $# -eq 0 ]; then
+    ISO_TO_USE="bin/$(find bin/ | sort -r | head -n 1)"
+else
+    ISO_TO_USE="$1"
+fi
+
+run_archiso -i "$ISO_TO_USE"
