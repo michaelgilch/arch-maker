7ffa2713ed240c52bef4d2cbaa1a8bbacafc507a
Michael Gilchrist <michaelgilch@gmail.com>
2021-03-10 15:52:48 -0500

    Lint with shellcheck

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,16 +3,16 @@
 # Finish installing Arch Linux via customization scripts
 
 function run_script() {
-    /bin/bash post-install/"$1"
-    if [ $? -ne 0 ]; then
-        echo "Ran into some problems with "$1". Exiting."
+    #/bin/bash post-install/"$1"
+    if /bin/bash post-install/"$1"; then
+        echo "Ran into some problems with $1. Exiting."
         exit 1
     fi
 }
 
 for f in [0-9][0-9]-*.sh
 do
-    if [ $1 == 'test' ]; then
+    if [ "$1" == 'test' ]; then
         echo "$f"
     else
         run_script "$f"
--- scripts/post-install/10-home.sh
+++ scripts/post-install/10-home.sh
@@ -9,12 +9,12 @@ ln -s /storage/music ~/music
 ln -s /storage/videos ~/videos
 
 # Setup dotfiles
-pushd /storage/dev/github
+pushd /storage/dev/github || exit
 if [ ! -d "/storage/dev/github/dotfiles" ]; then
     git clone git@github.com:michaelgilch/dotfiles.git
 fi
-pushd dotfiles
+pushd dotfiles || exit
 /bin/bash setup.sh
-popd;popd
+popd || exit; popd || exit
 
 
