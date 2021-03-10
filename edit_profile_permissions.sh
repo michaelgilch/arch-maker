a1ef243f20c614587713d6322a8e665063e5224a
Michael Gilchrist <michaelgilch@gmail.com>
2021-03-10 14:17:45 -0500

    Change permissions recursively for scripts folder
    - Requires custom build of archiso's mkarchiso
    - Merge submitted for fix in gitlab archiso, issue #100

--- edit_profile_permissions.sh
+++ edit_profile_permissions.sh
@@ -8,11 +8,5 @@ sed -i '/file_permissions/a ["/root/post-install.sh"]="0:0:755"' build/releng/pr
 
 # Use custom build of mkarchiso via dev/gitlab/archiso
 # to allow recursive setting of permissions in a folder
-sed -i '/file_permissions/a ["/root/post-install"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/00-verify-mounts.sh"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/05-services.sh"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/10-home.sh"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/30-aur-packages.sh"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/40-other-packages.sh"]="0:0:755"' build/releng/profiledef.sh
-#sed -i '/file_permissions/a ["/root/post-install/50-secrets.sh"]="0:0:755"' build/releng/profiledef.sh
+sed -i '/file_permissions/a ["/root/post-install"]="1000:1000:770"' build/releng/profiledef.sh
 
