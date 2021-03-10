bee68e8c531ff09650f400d895d334b91d5c7a42
Michael Gilchrist <michaelgilch@gmail.com>
2021-03-10 15:47:54 -0500

    Lint with shellcheck

--- scripts/post-install/30-aur-packages.sh
+++ scripts/post-install/30-aur-packages.sh
@@ -2,79 +2,79 @@
 # 
 # Install AUR packages and other packages from non-Arch sources
 
-pushd ~/dev/aur_pkgs
+pushd ~/dev/aur_pkgs || exit
 
 #
 # Google Chrome
 #
 if [ ! -d google-chrome ]; then
     git clone https://aur.archlinux.org/google-chrome.git
-    pushd google-chrome
+    pushd google-chrome || exit
 else
-    pushd google-chrome
+    pushd google-chrome || exit
     git pull
 fi
 makepkg -si --noconfirm
-popd
+popd || exit
 
 #
 # LXDM Themes
 #
 if [ ! -d lxdm-themes ]; then
     git clone https://aur.archlinux.org/lxdm-themes.git
-    pushd lxdm-themes
+    pushd lxdm-themes || exit
 else
-    pushd lxdm-themes
+    pushd lxdm-themes || exit
     git pull
 fi
 makepkg -si --noconfirm
 sudo sed -i 's/theme=Industrial/theme=Archlinux/g' /etc/lxdm/lxdm.conf
 sudo sed -i 's:# session=/usr/bin/startlxde:session=/usr/bin/openbox-session:' /etc/lxdm/lxdm.conf
 sudo sed -i 's/# numlock=0/numlock=1/' /etc/lxdm/lxdm.conf
-popd
+popd || exit
 
 #
 # Arch-Silence Grub Theme
 #
 if [ ! -d arch-silence ]; then
     git clone https://aur.archlinux.org/arch-silence-grub-theme-git.git
-    pushd arch-silence-grub-theme-git
+    pushd arch-silence-grub-theme-git || exit
 else
-    pushd arch-silence-grub-theme-git
+    pushd arch-silence-grub-theme-git || exit
     git pull
 fi
 makepkg -si --noconfirm
 sudo sed -i 's:#GRUB_THEME="/path/to/gfxtheme":GRUB_THEME="/boot/grub/themes/arch-silence/theme.txt":' /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-popd
+popd || exit
 
 #
 # Dropbox
 #
 if [ ! -d dropbox ]; then
     git clone https://aur.archlinux.org/dropbox.git
-    pushd dropbox
+    pushd dropbox || exit
 else
-    pushd dropbox
+    pushd dropbox || exit
     git pull
 fi
 wget https://linux.dropbox.com/fedora/rpm-public-key.asc
 gpg --import rpm-public-key.asc
 makepkg -si --noconfirm
-popd
+popd || exit
 
 # 
 # ReMarkable Client
 #
 if [ ! -d remarkable-client ]; then
     git clone https://aur.archlinux.org/remarkable-client.git
-    pushd remarkable-client
+    pushd remarkable-client || exit
 else
-    pushd remarkable-client
+    pushd remarkable-client || exit
     git pull
 fi
 makepkg -si --noconfirm
-popd
+popd || exit
 
 
 #
@@ -82,41 +82,41 @@ popd
 #
 if [ ! -d spotify ]; then
     git clone https://aur.archlinux.org/spotify.git
-    pushd spotify
+    pushd spotify || exit
 else
-    pushd spotify
+    pushd spotify || exit
     git pull
 fi
 gpg --recv-key D1742AD60D811D58
 makepkg -si
-popd
+popd || exit
 
 #
 # Slack
 # 
 if [ ! -d slack-desktop ]; then
     git clone https://aur.archlinux.org/slack-desktop.git
-    pushd slack-desktop
+    pushd slack-desktop || exit
 else
-    pushd slack-desktop
+    pushd slack-desktop || exit
     git pull
 fi
 makepkg -si
-popd 
+popd || exit
 
 # 
 # H2 Database
 #
 if [ ! -d h2 ]; then
     git clone https://aur.archlinux.org/h2.git
-    pushd h2
+    pushd h2 || exit
 else
-    pushd h2
+    pushd h2 || exit
     git pull
 fi
 makepkg -si
-popd 
+popd || exit
 
 # INSERT NEW HERE
 
-popd
+popd || exit
