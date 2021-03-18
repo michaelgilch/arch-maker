df2955c14e25ea144b524e0d35e8642cad6a9b4b
Michael Gilchrist <michaelgilch@gmail.com>
2021-03-18 14:28:25 -0400

    Cleanup configuration scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 #
-# Library used to obtain information from the user and hardware for the installation.
+# Library used to obtain information for the installation.
+#
+# Sources of information include, in order:
+#   - Configuration File
+#   - Hardware Probes
+#   - User Prompts
+# 
+# An entry in a configuration file will will take precidence over a user
+# prompt and/or hardware probe, preventing the latter from occuring.
 
-BASE_PKGS="base linux linux-firmware"
-
+#---------------------------------------
+# Prompts user for hostname if it does not exist in configuration file.
+# Globals:
+#   HOST_NAME
+#---------------------------------------
 function get_hostname() {
     if [ "$HOST_NAME" == "" ]; then
         read -r -p "Hostname: " HOST_NAME
@@ -11,11 +22,13 @@ function get_hostname() {
     echo "-> Hostname set to '$HOST_NAME'"
 }
 
-#------------------------------------------------------------------------------
-# Obtains password for specified user
+#---------------------------------------
+# Obtains password for specified user.
+# Globals: 
+#   PW_1, PW_2
 # Arguments:
-#   user to prompt for password
-#------------------------------------------------------------------------------
+#   Username to prompt for password
+#---------------------------------------
 function get_password() {
     PW_1=""
     PW_2=""
@@ -35,6 +48,11 @@ function get_password() {
     echo "-> $user Password set."
 }
 
+#---------------------------------------
+# Helper to obtain password for root user.
+# Globals:
+#   ROOT_PW
+#---------------------------------------
 function get_root_password() {
     if [ "$ROOT_PW" == "" ]; then
         get_password root
@@ -42,6 +60,11 @@ function get_root_password() {
     fi
 }
 
+#---------------------------------------
+# Obtains primary username if not set in config file.
+# Globals:
+#   PRIMARY_USER
+#---------------------------------------
 function get_primary_user() {
     if [ "$PRIMARY_USER" == "" ]; then
         read -r -p "Primary User Name: " PRIMARY_USER
@@ -49,6 +72,12 @@ function get_primary_user() {
     echo "-> Primary User set to '$PRIMARY_USER'."
 }
 
+#---------------------------------------
+# Helper to obtain password for primary user.
+# Globals:
+#   PRIMARY_USER
+#   PRIMARY_USER_PW
+#---------------------------------------
 function get_primary_user_password() {
     if [ "$PRIMARY_USER" != "" ]; then
         if [ "$PRIMARY_USER_PW" == "" ]; then
@@ -58,41 +87,49 @@ function get_primary_user_password() {
     fi
 }
 
+#---------------------------------------
+# Determines if installation is virtual, if not set in config file.
+# Globals:
+#   IS_VIRTUALBOX
+#---------------------------------------
 function get_is_virtualbox() {
     if [ "$IS_VIRTUALBOX" == "" ]; then
         IS_VIRTUALBOX="false"
-        if [ -n "$(lspci | grep -i virtualbox)" ]; then
+        if lspci | grep -qi virtualbox; then
             IS_VIRTUALBOX="true"
         fi
     fi
     echo "-> Virtualbox Install set to '$IS_VIRTUALBOX'"
 }
 
+#---------------------------------------
+# Determines if CPU is Intel, if not set in config file.
+# Globals:
+#   IS_INTEL_CPU
+#---------------------------------------
 function get_is_intel_cpu() {
     if [ "$IS_INTEL_CPU" == "" ]; then
         IS_INTEL_CPU="false"
-        if [ -n "$(lscpu | grep GenuineIntel)" ]; then
+        if lscpu | grep -qi GenuineIntel; then
             IS_INTEL_CPU="true"
         fi
     fi    
     echo "-> Intel CPU set to '$IS_INTEL_CPU'"
 }
 
+#---------------------------------------
+# Determines if graphics card is NVIDIA, if not set in config file.
+# Globals:
+#   IS_NVIDIA_GRAPHICS
+#---------------------------------------
 function get_is_nvidia_graphics() {
     if [ "$IS_NVIDIA_GRAPHICS" == "" ]; then
         IS_NVIDIA_GRAPHICS="false"
-        if [ -n "$(lspci | grep NVIDIA)" ]; then
+        if lspci | grep -qi NVIDIA; then
             IS_NVIDIA_GRAPHICS="true"
         fi
     fi    
     echo "-> NVIDIA Graphics set to '$IS_NVIDIA_GRAPHICS'"
-}
-
-function get_custom_packages() {
-    if [ -f "packages_${HOST_NAME,,}.sh" ]; then
-        echo "-> Found custom package list for ${HOST_NAME}"
-        source packages_"${HOST_NAME,,}".sh
-    fi
 }
 
 echo "Collecting configuration information..."
